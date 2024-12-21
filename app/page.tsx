21cd913f8b3f6869ba8f99c7c5784c4fa9232b81
DS-Garnett-Dan-Nu <garnett.dn@gmail.com>
2024-12-21 10:14:48 +0700

    Resized Image, Added Two Lines

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,17 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
+          //className="dark:invert"
           src="/heckerman.jpg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          alt="Heckerman Cooking"
+          width={380}
+          height={238}
           priority
         />
+        <h1>Bro's computer bouta'get COOKED!</h1>
+        <h1>Mu ha ha ha ha !!!</h1>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
